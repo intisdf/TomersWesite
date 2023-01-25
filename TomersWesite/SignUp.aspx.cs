2841e00f5592d5aad2bff9c3a137ff1f1b811a52
Lab314 <Lab314@314-CS01>
2023-01-25 14:55:41 +0200

    Data

--- TomersWesite/SignUp.aspx.cs
+++ TomersWesite/SignUp.aspx.cs
@@ -11,6 +11,8 @@ namespace TomersWesite
     public partial class SignUp : System.Web.UI.Page
     {
         public string st = "";
+        public string msg = "";
+        public string sqlMsg = "";
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Request.Form["submit"] != null)
@@ -22,9 +24,16 @@ namespace TomersWesite
                 string email = Request.Form["email"];
                 string hobies = Request.Form["hobies"].ToString();
 
+                string prefix = "05";
+                string phone = "2435493";
+                string city = "city";
+                string yearBorn = "1234";
+                int yBorn = int.Parse(yearBorn);
+                string pw = "111111";
+
                 st = "<table border='1' dir= 'ltr'>";
 
-                st += "<tr><th colspan='2'> הפרטים שהתקבלו מהשרת<./td></tr>";
+                st += "<tr><th colspan='2'> הפרטים שהתקבלו מהשרת</tr>";
                 st += "<tr><td> user name:></td><td>" + uName + "</td></tr>";
                 st += "<tr><td> first name:></td><td>" + fName + "</td></tr>";
                 st += "<tr><td> last name:></td><td>" + lName + "</td></tr>";
@@ -38,6 +47,40 @@ namespace TomersWesite
                 st += "</table>";
 
                 st += "</table>";
+
+                char hob1 = 'F';
+                char hob2 = 'F';
+                char hob3 = 'F';
+                char hob4 = 'F';
+                char hob5 = 'F';
+
+                //--- בדיקה: האם מחרוזת התחביבים מכילה את הערכים 1 - 5 ---
+                if (hobies.Contains('1')) hob1 = 'T';
+                if (hobies.Contains('2')) hob2 = 'T';
+                if (hobies.Contains('3')) hob3 = 'T';
+                if (hobies.Contains('4')) hob4 = 'T';
+                if (hobies.Contains('5')) hob5 = 'T';
+                string tableName = "usersTbl";
+
+                //--- בדיקה האם שם המשתמש תפוס ---
+                string sqlSelect = $"select * from {tableName} where uName = '{uName}'";
+                if (Helper.IsExist(sqlSelect))
+                {
+                    msg = "שם המשתמש תפוס, נסה שוב עם שם אחר";
+                    sqlMsg = sqlSelect;
+                }
+                else
+                {
+                    string sqlInsert = $"insert into {tableName} ";
+                    sqlInsert += $"values ('{uName}', N'{fName}', N'{lName}', '{email}', ";
+                    sqlInsert += $"'{gender}', N'{city}', {yBorn}, '{prefix}', '{phone}',  ";
+                    sqlInsert += $"'{hob1}', '{hob2}', '{hob3}', '{hob4}', '{hob5}', ";
+                    sqlInsert += $"'{pw}')";
+                    sqlMsg = sqlInsert;
+
+                    Helper.DoQuery(sqlInsert);
+                    msg = "הרשומה נוספה בהצלחה";
+                }
 
             }
 
